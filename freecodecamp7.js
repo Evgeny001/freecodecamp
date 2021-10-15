de2321107d48b86ac1f122064b0f238e4d16c182
Evgeny <info.elukas@gmail.com>
2021-10-15 13:51:18 +0300

    freecodecamp 15nov  add new 6 task

--- freecodecamp7.js
+++ freecodecamp7.js
@@ -16,3 +16,7 @@ var myArray = ["peanut butter", 0];
 
 // Only change code below this line
 var myArray = [["Bulls", 23], ["White Sox", 45]];
+
+var myArray = [50,60,70];
+myArray [1] ;
+var myData = myArray[0] ;
