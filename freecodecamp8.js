b6a8dc76ec4e6ab7607cfc2ae179416bb6711d6a
Evgeny <info.elukas@gmail.com>
2021-10-16 22:49:11 +0300

    16nov add 4 task

--- freecodecamp8.js
+++ freecodecamp8.js
@@ -9,3 +9,12 @@ var myArray = [["John", 23], ["dog", 3]];
 
 // Only change code below this line
 var removedFromMyArray = myArray.shift() ;
+
+// new task
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul",35]);
+
+// Only change code below this line
